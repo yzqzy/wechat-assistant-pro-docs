fc86baf4aeac082c132cd295c45080289939cdcb
杨志强 <yangzhiqiang@winwin-inc.com>
2024-10-08 17:49:59 +0800

    docs: update

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,46 +1,46 @@
-import clsx from "clsx";
-import Heading from "@theme/Heading";
-import styles from "./styles.module.css";
+import clsx from 'clsx'
+import Heading from '@theme/Heading'
+import styles from './styles.module.css'
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
-  description: JSX.Element;
-};
+  title: string
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>
+  description: JSX.Element
+}
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "通讯录",
-    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    title: '通讯录',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        通讯录功能可以帮助你快速找到你需要的人，同时也能帮助你管理好你的好友关系。支持导出通讯录、删除好友、修改备注等功能。
+        通讯录功能可以帮助你查看、管理你的微信好友，包括查看好友信息、添加好友、删除好友、修改备注、发送消息等。
       </>
-    ),
+    )
   },
   {
-    title: "群管理",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    title: '群管理',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        群管理功能可以帮助你管理你的群组，包括创建、邀请、删除、修改群组信息、导出群成员信息、群列表信息等。
+        群管理功能可以帮助你管理你的群组，例如查看群成员、群发消息、邀请好友入群、修改群名称、设置群公告等。
       </>
-    ),
+    )
   },
   {
-    title: "定时提醒",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    title: '定时提醒',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
         定时提醒功能可以帮助你安排好自己的日程，提醒自己及时完成任务。支持发送图片、语音、视频、文件等多种消息。
       </>
-    ),
-  },
-];
+    )
+  }
+]
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -49,7 +49,7 @@ function Feature({ title, Svg, description }: FeatureItem) {
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures(): JSX.Element {
@@ -63,5 +63,5 @@ export default function HomepageFeatures(): JSX.Element {
         </div>
       </div>
     </section>
-  );
+  )
 }
